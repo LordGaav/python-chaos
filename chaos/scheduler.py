1d7825104325b5870795bc4595f29401dee1deab
Nick Douma <n.douma@nekoconeko.nl>
2014-03-24 12:06:59 +0100

    Add Scheduler and Threads classes

---
+++ chaos/scheduler.py
@@ -0,0 +1,78 @@
+# Copyright (c) 2014 Nick Douma < n.douma [at] nekoconeko . nl >
+#
+# This file is part of chaos, a.k.a. python-chaos .
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3.0 of the License, or (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library. If not, see 
+# <http://www.gnu.org/licenses/>.
+
+import threading, datetime, time, logging
+
+class Scheduler(threading.Thread):
+	def __init__(self, delay, action, name, startNow=False, *args, **kwargs):
+		self.logger = logging.getLogger("{0}.{1}".format(__name__, name))
+		super(Scheduler, self).__init__(None, None, name, None, None)
+
+		self.delay = delay
+		self.main_action = action
+		self.name = name
+		self.main_args = args
+		self.main_kwargs = kwargs
+
+		self.stop = False
+		now = datetime.datetime.now()
+		if startNow is True:
+			self.lastRun = datetime.datetime.min
+			self.logger.debug("Thread {0} will start immediately".format(name))
+		else:
+			self.lastRun = now
+			if isinstance(startNow, (int, long)):
+				self.lastRun += datetime.timedelta(seconds=startNow)
+			wait = (self.lastRun - now).seconds + delay
+			self.logger.debug("Thread {0} will start in {1} seconds".format(name, wait))
+	
+	def setStartAction(self, action, *args, **kwargs):
+		self.init_action = action
+		self.init_args = args
+		self.init_kwargs = kwargs
+	
+	def setStopAction(self, action, *args, **kwargs):
+		self.stop_action = action
+		self.stop_args = args
+		self.stop_kwargs = kwargs
+
+	def run(self):
+		self.logger.debug("Thread {0} is entering main loop".format(self.name))
+
+		if hasattr(self, "init_action"):
+			self.logger.debug("Thread {0} is calling its init action")
+			self.init_action(*self.init_args, **self.init_kwargs)
+
+		while not self.stop:
+			self.logger.debug("Delay is {0}".format(self.delay))
+			if (datetime.datetime.now() - self.lastRun).total_seconds() > self.delay:
+				self.logger.debug("Thread {0} is running".format(self.name))
+				try:
+					self.main_action(*self.main_args, **self.main_kwargs)
+				except Exception:
+					self.logger.exception("Thread {0} generated an exception!".format(self.name))
+
+				self.lastRun = datetime.datetime.now()
+				self.logger.debug("Thread {0} is done".format(self.name))
+			time.sleep(1)
+
+		if hasattr(self, "stop_action"):
+			self.logger.debug("Thread {0} is calling its stop action")
+			self.stop_action(*self.stop_args, **self.stop_kwargs)
+
+		self.logger.debug("Thread {0} is exiting main loop".format(self.name))
---
+++ chaos/threads.py
@@ -0,0 +1,57 @@
+# Copyright (c) 2014 Nick Douma < n.douma [at] nekoconeko . nl >
+#
+# This file is part of chaos, a.k.a. python-chaos .
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3.0 of the License, or (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library. If not, see 
+# <http://www.gnu.org/licenses/>.
+
+import threading, logging
+
+class Threads(object):
+	thread_list = {}
+
+	def __init__(self):
+		self.logger = logging.getLogger(__name__)
+
+	def registerThread(self, name, thread):
+		if not isinstance(thread, threading.Thread):
+			self.logger.error("Thread {0} is not actually a Thread!".format(name))
+			raise Exception("Thread {0} is not actually a Thread!".format(name))
+
+		if name in self.thread_list:
+			self.logger.error("Thread {0} already registered!".format(name))
+			raise Exception("Thread {0} already registered!".format(name))
+
+		self.thread_list[name] = thread
+		self.logger.debug("Registered thread {0}".format(name))
+
+		return thread
+	
+	def getThreads(self):
+		return self.thread_list.keys()
+
+	def getThread(self, name):
+		if not name in self.thread_list:
+			self.logger.error("Thread {0} is not registered!".format(name))
+			raise Exception("Thread {0} is not registered!".format(name))
+
+		return self.thread_list[name]
+
+	def unregisterThread(self, name):
+		if not name in self.thread_list:
+			self.logger.error("Thread {0} is not registered!".format(name))
+			raise Exception("Thread {0} is not registered!".format(name))
+
+		del self.thread_list[name]
+		self.logger.debug("Unregistered thread {0}".format(name))
