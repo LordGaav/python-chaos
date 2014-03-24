0a91489ac194fd882e7cfe22a02dfda92d9b62b9
Nick Douma <n.douma@nekoconeko.nl>
2014-03-24 12:06:47 +0100

    Add Package metadata

---
+++ README.md
@@ -0,0 +1,37 @@
+python-chaos
+------------
+
+A set of libaries and subroutines I commonly use in my python projects.
+
+Dependencies
+============
+
+* python >= 2.7
+
+Building
+========
+
+To build a Debian package, perform the following steps:
+
+1. `apt-get install ubuntu-dev-tools debhelper dh-exec`
+
+From here you can either build the package with pbuilder-dist:
+
+2. `pbuilder-dist saucy create`
+3. `make -f debian/Makefile source_no_sign`
+4. `make -f debian/Makefile pbuild CHANGES=../python-chaos_xxxx_.dsc`
+5. look for the resulting .deb in ~/pbuilder/saucy_result
+
+Or directly using dpkg-buildpackage
+
+2. `make -f debian/Makefile package`
+
+
+Installing
+==========
+
+Either use the methods described above to build your own package, or install it from my PPA.
+
+1. `add-apt-repository ppa:lordgaav/python-chaos`
+2. `apt-get update && apt-get install python-chaos`
+
---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# Copyright (c) 2014 Nick Douma < n.douma [at] nekoconeko . nl >
+#
+# This file is part of chaos, a.k.a. python-chaos .
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3.0 of the License, or (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library. If not, see 
+# <http://www.gnu.org/licenses/>.
+
+from distutils.core import setup
+
+NAME="chaos"
+VERSION="0.1"
+DESCRIPTION="Assorted libraries and subroutines"
+
+f = open(join(dirname(__file__), 'README.md'))
+long_description = f.read().strip()
+f.close()
+
+setup(
+	name=NAME,
+	version=VERSION,
+	description=DESCRIPTION,
+	long_description=long_description,
+	license="LGPL3",
+	author="Nick Douma",
+	author_email="n.douma@nekoconeko.nl",
+	url="https://github.com/LordGaav/python-chaos",
+	packages=["chaos"]
+)
