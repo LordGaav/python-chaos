3efd62cd2f61189d1f6a5cef3261984f9dead3fb
Nick Douma <n.douma@nekoconeko.nl>
2014-03-24 16:27:00 +0100

    Add file Globber

---
+++ chaos/globber.py
@@ -0,0 +1,46 @@
+# Copyright (c) 2014 Nick Douma < n.douma [at] nekoconeko . nl >
+#
+# This file is part of chaos, a.k.a. python-chaos .
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3.0 of the License, or (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library. If not, see 
+# <http://www.gnu.org/licenses/>.
+
+from __future__ import absolute_import
+import os, fnmatch
+
+class Globber(object):
+	"""
+	Traverses a directory and returns all absolute filenames.
+	"""
+
+	def __init__(self, path, filter=['*'], recursive=True):
+		""" Initialize Globber parameters. Filter may be a list of globbing patterns. """
+		self.path = path
+		self.filter = filter
+		self.recursive = recursive
+	
+	def glob(self):
+		"""
+		Traverse directory, and return all absolute filenames of files that
+		match the globbing patterns.
+		"""
+		matches = []
+		for root, dirnames, filenames in os.walk(self.path):
+			if not self.recursive:
+				while len(dirnames) > 0:
+					dirnames.pop()
+			for filter in self.filter:
+				for filename in fnmatch.filter(filenames, filter):
+					matches.append(os.path.join(root, filename))
+		return matches
