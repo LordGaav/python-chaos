f0aa3ce6afe2f5d28af9433970df44f65cff9f33
Nick Douma <n.douma@nekoconeko.nl>
2014-04-01 11:45:22 +0200

    Move Thread related stuff to separate sub package

--- chaos/threading/__init__.py
+++ chaos/threading/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # Copyright (c) 2014 Nick Douma < n.douma [at] nekoconeko . nl >
 #
 # This file is part of chaos, a.k.a. python-chaos .
@@ -16,26 +15,3 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library. If not, see 
 # <http://www.gnu.org/licenses/>.
-
-from os.path import join, dirname
-from distutils.core import setup
-
-NAME="chaos"
-VERSION="0.1"
-DESCRIPTION="Assorted libraries and subroutines"
-
-f = open(join(dirname(__file__), 'README.md'))
-long_description = f.read().strip()
-f.close()
-
-setup(
-	name=NAME,
-	version=VERSION,
-	description=DESCRIPTION,
-	long_description=long_description,
-	license="LGPL3",
-	author="Nick Douma",
-	author_email="n.douma@nekoconeko.nl",
-	url="https://github.com/LordGaav/python-chaos",
-	packages=["chaos", "chaos.threading"]
-)
