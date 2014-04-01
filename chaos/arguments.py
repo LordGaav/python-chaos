fb8d1b7b88195b009c936af4fed2f8ee0cc206b0
Nick Douma <n.douma@nekoconeko.nl>
2014-04-01 11:12:42 +0200

    Make suppressing get_config_argparse options more finegrained.

--- chaos/arguments.py
+++ chaos/arguments.py
@@ -24,7 +24,7 @@ from __future__ import absolute_import
 import logging
 from argparse import ArgumentParser
 
-def get_config_argparse(suppress_help=False):
+def get_config_argparse(suppress=[]):
 	"""
 	Create an ArgumentParser which listens for the following common options:
 	- --config
@@ -35,19 +35,28 @@ def get_config_argparse(suppress_help=False):
 
 	Arguments
 	---------
-	suppress_help: boolean
-		If set to True, help information is suppressed in the returned ArgumentParser
-		If set to False, help information is automatically output by ArgumentParser 
+	suppress_help: list of strings
+		Defines what options to suppress from being added to the ArgumentParser. Provide
+		the name of the options to suppress, without "--". Of note in particular is the help option.
+
+		If help is suppressed, help information is suppressed in the returned ArgumentParser
+		If help is not suppressed, help information is automatically output by ArgumentParser 
 			when -h/--help is passed, and the program is exited upon parsing the arguments.
+		In both cases, -h/--help is not actually suppressed, only the behaviour of the ArgumentParser
+		when encountering the option.
 	"""
 
-	config_parser = ArgumentParser(description="Looking for config", add_help=(not suppress_help))
-	config_parser.add_argument("--config",	metavar="CFG",		 type=str,		help="Config file to load")
-	if suppress_help:
+	config_parser = ArgumentParser(description="Looking for config", add_help=(not "help" in suppress))
+	if not "config" in suppress:
+		config_parser.add_argument("--config",	metavar="CFG",		 type=str,		help="Config file to load")
+	if "help" in suppress:
 		config_parser.add_argument("--help",	action="store_true", default=False, help="Display usage information and exit")
-	config_parser.add_argument("--quiet",	action="store_true", default=False, help="Don't print messages to stdout")
-	config_parser.add_argument("--verbose", action="store_true", default=False, help="Output debug messages")
-	config_parser.add_argument("--version", action="store_true", default=False, help="Display version information and exit")
+	if not "quiet" in suppress:
+		config_parser.add_argument("--quiet",	action="store_true", default=False, help="Don't print messages to stdout")
+	if not "verbose" in suppress:
+		config_parser.add_argument("--verbose", action="store_true", default=False, help="Output debug messages")
+	if not "version" in suppress:
+		config_parser.add_argument("--version", action="store_true", default=False, help="Display version information and exit")
 
 	return config_parser
 
@@ -65,4 +74,4 @@ def get_config_arguments():
 	logger = logging.getLogger(__name__)
 	logger.debug("Parsing configuration arguments")
 
-	return get_config_argparse(suppress_help=True).parse_known_args()
+	return get_config_argparse(suppress=["help"]).parse_known_args()
